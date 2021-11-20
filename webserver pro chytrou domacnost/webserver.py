e207fdcd52ce2e0a33a8ea74659d672f71440aec
Lubomir Jurcisin <75535869+LubJur@users.noreply.github.com>
2021-11-20 19:21:49 +0100

    zmena farby pri zapade slnka

--- webserver pro chytrou domacnost/webserver.py
+++ webserver pro chytrou domacnost/webserver.py
@@ -17,7 +17,6 @@ import requests
 from json import loads
 from datetime import datetime
 import time
-from uuid import uuid4
 
 app = Flask("webserver")
 
@@ -26,13 +25,14 @@ lights_id = {"obyvakLight": 0, "koupelnaLight": 0, "kuchyneLight": 0, "karsobLig
 switches_id = {"obyvakSwitch": 0, "koupelnaSwitch": 0, "kuchyneSwitch": 0, "karsobSwitch": 0, "karlikSwitch": 0,
            "karlosSwitch": 0, "juliaSwitch": 0}
 motion_id = {"obyvakMotion": 0, "kuchyneMotion": 0}
-"""
+
+
 for light in lights_id:
     print(light)
     get_response = requests.get("https://home_automation.iamroot.eu/newSmartLight")
     id = loads(get_response.text)["id"]
     lights_id.update({light: id})
-
+"""
 for switch in switches_id:
     print(switch)
     get_response = requests.get("https://home_automation.iamroot.eu/newSwitchSensor")
@@ -48,8 +48,32 @@ for motion in motion_id:
 """
 
 print(datetime.now().time())  # https://www.programiz.com/python-programming/datetime/current-time
-if datetime.now().time()[:2] > 17:
-    color = 6500
+sunset = 18
+sunrise = 6
+proportion_color = 4200
+now_time = str(datetime.now().time())
+now_time = int(now_time[:2])*60 + int(now_time[3:5])
+print(now_time)
+if now_time > sunset*60:  # 17:00
+    time_before = now_time
+    while now_time < sunset*60 + 90:  # 18:30
+        now_time = str(datetime.now().time())
+        now_time = int(now_time[:2]) * 60 + int(now_time[3:5])
+        if time_before + 1 < now_time:
+            time_before = now_time
+            proportion_color -= 47 #235
+            for light in lights_id:
+                id = lights_id[light]
+                time.sleep(0.2)
+                print("idem poslat request")
+                get_response = requests.get(f"https://home_automation.iamroot.eu/device/{id}/color_temperature/{2300+proportion_color}")
+                print(loads(get_response.text))
+
+
+
+    minutes = datetime.now().time()
+    print(minutes)
+
 
 
 for i in lights_id.items():
