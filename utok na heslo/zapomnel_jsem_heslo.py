468e57cef03088e2f25581451d860832e3281dd8
LubJur <75535869+LubJur@users.noreply.github.com>
2021-12-23 13:03:33 +0100

    brute force attack na zip

---
+++ utok na heslo/zapomnel_jsem_heslo.py
@@ -0,0 +1,21 @@
+import pyzipper
+import string
+import itertools
+
+alphabet = list(string.ascii_lowercase)
+
+# https://www.delftstack.com/howto/numpy/python-numpy-combinations/
+combinations = itertools.product(alphabet, alphabet, alphabet)
+
+with pyzipper.AESZipFile("staryarchiv.zip") as my_zip:
+    for combination in combinations:
+        combination = "".join(combination)
+        print("Trying:", combination)
+        password = bytes(combination, encoding="utf8")
+        try:
+            my_zip.extractall(pwd=password)
+        except:
+            pass
+        else:
+            print("The password is:", combination)
+            break
